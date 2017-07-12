67722e3ebb339c3fa2a1443b5ff88d784e84c091
Wim Lavrijsen <WLavrijsen@lbl.gov>
2017-07-12 08:57:05 -0700

    introduce setup.py script

---
+++ setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import os, glob
+from setuptools import setup, Extension
+from codecs import open
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='CPyCppyy',
+    version='0.1.0',
+    description='Cling-based Python-C++ bindings',
+    long_description=long_description,
+
+    url='http://doc.pypy.org/en/latest/cppyy.html',
+
+    # Author details
+    author='Wim Lavrijsen',
+    author_email='WLavrijsen@lbl.gov',
+
+    license='LBNL BSD',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Developers',
+
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Interpreters',
+
+        #'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: C',
+        'Programming Language :: C++',
+
+        'Natural Language :: English'
+    ],
+
+    keywords='C++ bindings',
+
+    ext_modules=[Extension('libcppyy',
+        sources=glob.glob('src/*.cxx'),
+        extra_compile_args=['-std=c++11', '-O2'],
+        include_dirs=['include'])],
+
+)
+
