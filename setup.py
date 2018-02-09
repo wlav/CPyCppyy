4c750f760a2baf8a0187ef41b8b44257c67566b3
Wim Lavrijsen <WLavrijsen@lbl.gov>
2018-02-09 10:19:09 -0800

    update to 0.9.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_cflags():
 
 setup(
     name='CPyCppyy',
-    version='0.8.8',
+    version='0.9.0',
     description='Cling-based Python-C++ bindings for CPython',
     long_description=long_description,
 
