1dc8607b8812d8bcc7e8ec90babd84506dc6c541
Wim Lavrijsen <WLavrijsen@lbl.gov>
2018-05-15 13:25:16 -0700

    add initializer

--- src/CPPScope.cxx
+++ src/CPPScope.cxx
@@ -137,6 +137,7 @@ static PyObject* pt_new(PyTypeObject* subtype, PyObject* args, PyObject* kwds)
     if (!result)
         return nullptr;
 
+    result->fFlags      = CPPScope::kNone;
     result->fCppObjects = new CppToPyMap_t;
     result->fModuleName = nullptr;
 
