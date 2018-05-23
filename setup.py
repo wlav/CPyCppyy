d326d84e6b4042fcc9ea6abd977a34de06f2b93f
Wim Lavrijsen <WLavrijsen@lbl.gov>
2018-05-22 19:03:48 -0700

    patch release for improved STL pythonizations

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ class my_bdist_wheel(_bdist_wheel):
 
 setup(
     name='CPyCppyy',
-    version='1.0.0',
+    version='1.0.1',
     description='Cling-based Python-C++ bindings for CPython',
     long_description=long_description,
 
