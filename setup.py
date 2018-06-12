5870f8e2b0b76306152d286f285dc7d76a1f8d94
Wim Lavrijsen <WLavrijsen@lbl.gov>
2018-06-12 16:18:20 -0700

    patch release for intializer_list and vector initialization

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ class my_bdist_wheel(_bdist_wheel):
 
 setup(
     name='CPyCppyy',
-    version='1.1.0',
+    version='1.1.1',
     description='Cling-based Python-C++ bindings for CPython',
     long_description=long_description,
 
