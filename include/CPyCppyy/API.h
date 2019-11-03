1446c89ba6c98ca9a85b170846dd808e7024b5e8
Wim Lavrijsen <WLavrijsen@lbl.gov>
2019-11-02 17:51:13 -0700

    remove spurious chars

--- include/CPyCppyy/API.h
+++ include/CPyCppyy/API.h
@@ -11,9 +11,9 @@
 
 // Python
 #ifdef _WIN32
-#pragma warning (disable : 4275)\n"
-#pragma warning (disable : 4251)\n"
-#pragma warning (disable : 4800)\n"
+#pragma warning (disable : 4275)
+#pragma warning (disable : 4251)
+#pragma warning (disable : 4800)
 #endif
 #if defined(linux)
 #include <stdio.h>
