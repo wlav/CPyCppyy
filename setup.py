0c0caad8e0837cb178d9f25a8c35bbb087433ffd
Wim Lavrijsen <WLavrijsen@lbl.gov>
2019-12-27 21:55:07 -0800

    start new patch version

--- setup.py
+++ setup.py
@@ -118,7 +118,7 @@ class MyDistribution(Distribution):
 
 setup(
     name='CPyCppyy',
-    version='1.10.0',
+    version='1.10.1',
     description='Cling-based Python-C++ bindings for CPython',
     long_description=long_description,
 
