f34de8f402890176f9bbc07d55a8e7621b02e6e8
Wim Lavrijsen <WLavrijsen@lbl.gov>
2020-09-11 20:03:11 -0700

    up version

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ cmdclass = {
 
 setup(
     name='CPyCppyy',
-    version='1.11.3',
+    version='1.11.4',
     description='Cling-based Python-C++ bindings for CPython',
     long_description=long_description,
 
